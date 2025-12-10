e1c42c4eb2535fcb8e6bc6221a379f97eda36d16
Maxim <prodbyyoungwilly@gmail.com>
2025-12-10 22:09:26 +0300

    Deleted comments

--- app/views.py
+++ app/views.py
@@ -6,16 +6,12 @@ import os
 
 def home_view(request):
     template_name = 'app/home.html'
-    # впишите правильные адреса страниц, используя
-    # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
         'Показать текущее время': reverse('time'),
         'Показать содержимое рабочей директории': reverse('workdir'),
     }
-    
-    # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
+
     context = {
         'pages': pages
     }
@@ -23,17 +19,12 @@ def home_view(request):
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
-    # возвращается просто текст
     current_time = datetime.datetime.now().time()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
 
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
     files = os.listdir()
     msg = "Содержимое рабочей директории:<br>" + "<br>".join(files)
     return HttpResponse(msg)
